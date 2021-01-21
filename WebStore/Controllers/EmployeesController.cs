cd2e026eb0d17f60947da3bb04e35613cdd74e62
Евреинов Дмитрий <d.evreinov@outlook.com>
2021-01-21 21:49:29 +0300

    Детализация соотрудника.

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -12,5 +13,13 @@ namespace WebStore.Controllers
             _Employees = Data.TestData.Employees;
         }
         public IActionResult Index() => View(_Employees);
+
+        public IActionResult Details(int id)
+        {
+            var employee = _Employees.FirstOrDefault(e => e.Id == id);
+            if (employee is not null)
+                return View(employee);
+            return NotFound();
+        }
     }
 }
