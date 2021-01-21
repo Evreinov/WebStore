749a1af8dc1307a74b12b25633c3a0b96cb67f07
Евреинов Дмитрий <d.evreinov@outlook.com>
2021-01-21 20:41:46 +0300

    Фиксация частичных представлений

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private List<Employee> _Employees;
+        public EmployeesController()
+        {
+            _Employees = Data.TestData.Employees;
+        }
+        public IActionResult Index() => View(_Employees);
+    }
+}
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class TestData
+    {
+        public static List<Employee> Employees 
+        { 
+            get;
+            set;
+        }
+
+        public static void Load()
+        {
+            if (Employees is null || Employees.Count == 0)
+            {
+                Employees = Enumerable.Range(1, 45)
+                    .Select(i => new Employee
+                    {
+                        Id = i,
+                        FirstName = $"Имя{i}",
+                        LastName = $"Фамилия{i}",
+                        Patronymic = $"Отчество{i}",
+                        ShortName = $"Фамилия{i} И. О.",
+                        Birthday = DateTime.Now.AddMonths(i * 2 - 560),
+                        Sex = i % 2 == 0 ? 0 : 1,
+                        Number = i.ToString("D5"),
+                        InternalPhone = $"1{i:D2}",
+                        HomePhone = $"+712345678{i:D2}",
+                        MobilePhone = $"+712345678{i:D2}",
+                        BusinessPhone = $"+712345678{i:D2}",
+                        Fax = $"+712345678{i:D2}",
+                        Email = $"user{i}@supermail.ru",
+                        ImagePath = i % 2 == 0 ? $"~/img/profile_female.png" : $"~/img/profile_male.png"
+                    })
+                    .ToList();
+            }
+        }
+    }
+}
