4d62e83f3cb183a434bf9f7c54fbca2dcc4d2748
Евреинов Дмитрий <d.evreinov@outlook.com>
2021-01-26 15:54:31 +0300

    Интерфейс и сервис для работы с сотрудниками.

---
+++ WebStore/Infrastructure/Interfaces/IEmloyeesData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    interface IEmloyeesData
+    {
+        IEnumerable<Employee> Get();
+
+        Employee Details(int id);
+
+        int Create(Employee employee);
+
+        void Edit(Employee employee);
+
+        bool Delete(int id);
+    }
+}
---
+++ WebStore/Infrastructure/Services/InMemoryEmployeesData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Services
+{
+    public class InMemoryEmployeesData : IEmloyeesData
+    {
+        private readonly List<Employee> _Employees;
+        private int _CurrentMaxId;
+        public InMemoryEmployeesData()
+        {
+            _Employees = Data.TestData.Employees;
+            _CurrentMaxId = _Employees.DefaultIfEmpty().Max(e => e?.Id ?? 1);
+        }
+        public IEnumerable<Employee> Get() => _Employees;
+        public Employee Details(int id) => _Employees.FirstOrDefault(e => e.Id == id);
+        public int Create(Employee employee)
+        {
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+            if (_Employees.Contains(employee)) return employee.Id;
+            employee.Id = ++_CurrentMaxId;
+            _Employees.Add(employee);
+            return employee.Id;
+        }
+        public void Edit(Employee employee)
+        {
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+            if (_Employees.Contains(employee)) return;
+            var item = Details(employee.Id);
+            if (item is null) return;
+            item.FirstName = employee.FirstName;
+            item.LastName = employee.LastName;
+            item.Patronymic = employee.Patronymic;
+            item.ShortName = employee.ShortName;
+            item.Birthday = employee.Birthday;
+            item.Sex = employee.Sex;
+            item.Number = employee.Number;
+            item.InternalPhone = employee.InternalPhone;
+            item.HomePhone = employee.HomePhone;
+            item.MobilePhone = employee.MobilePhone;
+            item.BusinessPhone = employee.BusinessPhone;
+            item.Fax = employee.Fax;
+            item.Email = employee.Email;
+            item.ImagePath = employee.ImagePath;
+        }
+
+        public bool Delete(int id)
+        {
+            var item = Details(id);
+            if (item is null) return false;
+            return _Employees.Remove(item);
+        }
+    }
+}
