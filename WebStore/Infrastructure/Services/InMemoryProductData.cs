b440e9ad9d2885c554c42c1c9053eeaddf9fcc91
Евреинов Дмитрий <d.evreinov@outlook.com>
2021-01-31 02:27:48 +0300

    Каталог продуктов.

--- WebStore/Infrastructure/Services/InMemoryProductData.cs
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Infrastructure.Interfaces;
 
@@ -10,5 +12,18 @@ namespace WebStore.Infrastructure.Services
         public IEnumerable<Brand> GetBrands() => Data.TestData.Brands;
 
         public IEnumerable<Section> GetSections() => Data.TestData.Sections;
+
+        public IEnumerable<Product> GetProducts(ProductFilter Filter)
+        {
+            var query = Data.TestData.Products;
+
+            if (Filter?.SectionId is { } section_id)
+                query = query.Where(product => product.SectionId == section_id);
+
+            if (Filter?.SectionId is { } brand_id)
+                query = query.Where(product => product.BrandId == brand_id);
+
+            return query;
+        }
     }
 }
