ec4d9fd9fe8bd9c57617e499f752b690bb3c8646
Евреинов Дмитрий <d.evreinov@outlook.com>
2021-01-31 23:55:44 +0300

    Контекст для БД

---
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL.Context
+{
+    class WebStoreDB : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options) { }
+    }
+}
