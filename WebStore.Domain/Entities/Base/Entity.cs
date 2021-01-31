694d90542f4e3b3522fa4b1cc6b22b524320ec1a
Евреинов Дмитрий <d.evreinov@outlook.com>
2021-01-31 23:42:56 +0300

    Реализация сущностей для использования их в контексте БД.

--- WebStore.Domain/Entities/Base/Entity.cs
+++ WebStore.Domain/Entities/Base/Entity.cs
@@ -1,9 +1,12 @@
-﻿using WebStore.Domain.Entities.Base.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities.Base
 {
     public abstract class Entity : IEntity
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Указываем, что поле является первичным ключем и уникальным (параметр не требуется, если поле называется Id)
         public int Id { get; set; }
     }
 }
