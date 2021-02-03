f2ab4cb61b9d2c962b7a8b1c7fb9acbde14fc380
Евреинов Дмитрий <d.evreinov@outlook.com>
2021-02-03 19:24:14 +0300

    Удаление старых файлов из мастер, и ошибок кодировки.

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,4 +1,4 @@
-ï»¿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,7 +16,6 @@ namespace WebStore
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 #if DEBUG
-            // ÐÐ°Ð³ÑÑÐ·ÐºÐ° ÑÐµÑÑÐ¾Ð²ÑÑ Ð´Ð°Ð½Ð½ÑÑ Ð² Ð¿Ð°Ð¼ÑÑÑ.
             Data.TestData.Load();
 #endif
 
@@ -30,21 +29,14 @@ namespace WebStore
                 app.UseBrowserLink();
             }
 
-            // ÐÐºÐ»ÑÑÐµÐ½Ð¸Ðµ Ð¾Ð±ÑÐ»ÑÐ¶Ð¸Ð²Ð°Ð½Ð¸Ñ ÑÑÐ°ÑÐ¸ÑÐµÑÐºÐ¸Ñ ÑÐ°Ð¹Ð»Ð¾Ð².
             app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseStaticFiles();   // äîáàâëÿåì ïîääåðæêó ñòàòè÷åñêèõ ôàéëîâ
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                //endpoints.MapGet("/", async context =>
-                //{
-                //    await context.Response.WriteAsync("Hello World!");
-                //});
-
-                // Ð¡ÑÐ°Ð½Ð´Ð°ÑÑÐ½Ð°Ñ Ð¼Ð°ÑÑÑÑÑÐ¸Ð·Ð°ÑÐ¸Ñ Ð¸ÑÐ¿Ð¾Ð»ÑÐ·ÑÐµÑÑÑ Ñ ÐºÐ¾Ð½ÑÑÐ¾Ð»Ð»ÐµÑÐ°Ð¼Ð¸ Ð¸ Ð¿ÑÐµÐ´ÑÑÐ°Ð²Ð»ÐµÐ½Ð¸ÑÐ¼Ð¸.
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
