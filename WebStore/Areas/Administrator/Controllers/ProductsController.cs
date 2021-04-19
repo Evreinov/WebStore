7da104c2526a58c597eaf085ab539bae11b7a745
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-04-19 15:56:10 +0300

    Заготовка области администратора.

--- WebStore/Areas/Administrator/Controllers/ProductsController.cs
+++ WebStore/Areas/Administrator/Controllers/ProductsController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using WebStore.Domain.Identity;
 
 namespace WebStore.Areas.Administrator.Controllers
 {
     [Area("Administrator"), Authorize(Roles = Role.Administrators)]
-    public class HomeController : Controller
+    public class ProductsController : Controller
     {
         public IActionResult Index()
         {
