8e86a5087db230c94c768996ea6e0021baa79f8d
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-04-26 08:12:37 +0300

    Клиент каталога товаров.

---
+++ Services/WebStore.Clients/Products/ProductsClient.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Net.Http;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.DTO;
+using WebStore.Interfaces;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Products
+{
+    class ProductsClient : BaseClient, IProductData
+    {
+        public ProductsClient(IConfiguration Configuration) : base(Configuration, WebAPI.Products) { }
+
+        public IEnumerable<BrandDTO> GetBrands() => Get<IEnumerable<BrandDTO>>($"{Address}/brands");
+
+        public ProductDTO GetProductById(int id) => Get<ProductDTO>($"{Address}/{id}");
+
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter = null) =>
+            Post(Address, Filter ?? new ProductFilter())
+            .Content
+            .ReadAsAsync<IEnumerable<ProductDTO>>()
+            .Result;
+
+        public IEnumerable<SectionDTO> GetSections() => Get<IEnumerable<SectionDTO>>($"{Address}/sections");
+    }
+}
