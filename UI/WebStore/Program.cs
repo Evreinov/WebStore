db698b53292531525f7e20b9bc72c13e5c1cce54
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-04-30 10:32:32 +0300

    Первый способ конфигурирования ситстемы логирования (жесткий).

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,17 +1,22 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace WebStore
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                //.ConfigureLogging((host, log) => log
+                //    .ClearProviders()
+                //    .AddEventLog()
+                //    .AddConsole()
+                //    .AddFilter/*<ConsoleLoggerProvider>*/("Microsoft.Hosting", LogLevel.Error)
+                //    .AddFilter((category, level) => !(category.StartsWith("Microsoft") && level >= LogLevel.Warning)))
                 .ConfigureWebHostDefaults(webBuilder =>
                     webBuilder.UseStartup<Startup>()
                 );
