868aa35454c802230b98c1f68f10502c8964494a
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-05-11 19:30:41 +0300

    Опячатки

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -1,5 +1,5 @@
 ﻿Cart = {
-    _propertise: {
+    _properties: {
         getCartViewLink: "",
         addToCartLink: "",
         decrementLink: "",
@@ -7,7 +7,38 @@
     },
 
     init: function (properties) {
-        $.extend(Cart._propertise, properties);
+        $.extend(Cart._properties, properties);
 
+        $(".add-to-cart").click(Cart.addToCart);
+    },
+
+    addToCart: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function () {
+                Cart.showToolTip(button);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("addToCart fail"); });
+    },
+
+    showToolTip: function (button) {
+        button.tooltip({ title: "Добавлено в корзину!" }).tooltip("show");
+        setTimeout(function () {
+            button.tooltip("destroy");
+        }, 500);
+    },
+
+    refreshCartView: function () {
+        var container = $("#cart-container");
+        $.get(Cart._properties.getCartViewLink)
+            .done(function (CartHtml) {
+                container.html(CartHtml);
+            })
+            .fail(function () { console.log("refreshCartView fail"); });
     }
 }
