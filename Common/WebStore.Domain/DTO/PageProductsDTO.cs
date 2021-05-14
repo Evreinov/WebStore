500c977435a2798b8ae9979be65a35c7a18bf560
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-05-14 11:44:16 +0300

    Внедрение постраничного каталога товаров в сервис.

---
+++ Common/WebStore.Domain/DTO/PageProductsDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebStore.Domain.DTO
+{
+    public record PageProductsDTO(IEnumerable<ProductDTO> Products, int TotalCount);
+    //{ // Альтернативный метод создания свойств в record
+    //    public IEnumerable<ProductDTO> Products { get; init; }
+    //    public int TotalCount { get; init; }
+    //}
+}
--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System.Linq;
 using WebStore.Domain;
 using WebStore.Domain.ViewModels;
@@ -10,14 +11,25 @@ namespace WebStore.Controllers
     public class CatalogController : Controller
     {
         private readonly IProductData _ProductData;
-        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
+        private readonly IConfiguration _Configuration;
 
-        public IActionResult Index(int? BrandId, int? SectionId)
+        public CatalogController(IProductData ProductData, IConfiguration Configuration)
         {
+            _ProductData = ProductData;
+            _Configuration = Configuration;
+        }
+
+        public IActionResult Index(int? BrandId, int? SectionId, int Page = 1, int? PageSize = null)
+        {
+            var page_size = PageSize
+                ?? (int.TryParse(_Configuration["CatalogPageSize"], out var value ) ? value : null);
+
             var Filter = new ProductFilter
             {
                 BrandId = BrandId,
-                SectionId = SectionId
+                SectionId = SectionId,
+                Page = Page,
+                PageSize = page_size,
             };
 
             var products = _ProductData.GetProducts(Filter);
@@ -26,7 +38,7 @@ namespace WebStore.Controllers
             {
                 SectionId = SectionId,
                 BrandId = BrandId,
-                Products = products.OrderBy(p => p.Order).FromDTO().ToView()
+                Products = products.Products.OrderBy(p => p.Order).FromDTO().ToView()
             });
         }
 
